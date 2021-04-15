a7d17d9edaaa25c4f9b5281e32b1291bee7429d2
Christine Dodrill <me@christine.website>
2021-04-15 17:54:33 -0400

    it tests!
    Signed-off-by: Christine Dodrill <me@christine.website>

--- test.nix
+++ test.nix
@@ -19,5 +19,19 @@ in import "${pkgs}/nixos/tests/make-test-python.nix" ({ pkgs, ... }: {
 
   testScript = ''
     start_all()
+
+    machine.wait_for_console_text("printerfact")
+
+    chardev = [
+        x
+        for x in machine.wait_until_succeeds("cat /proc/devices").splitlines()
+        if "printerfact" in x
+    ][0].split(" ")[0]
+
+    machine.wait_until_succeeds("mknod /dev/printerfact c {} 1".format(chardev))
+    machine.wait_for_file("/dev/printerfact")
+
+    print(machine.wait_until_succeeds("stat /dev/printerfact"))
+    machine.wait_until_succeeds("cat /dev/printerfact")
   '';
 })
